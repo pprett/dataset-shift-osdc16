c9b7683296ad3a88f2a7c7422201f4ec343e76c4
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2016-10-03 00:00:21 +0200

    better doc strings

--- bokeh_checkerboard.py
+++ bokeh_checkerboard.py
@@ -1,3 +1,19 @@
+"""Interactive plot to illustrate the phenomenon of covariate shift.
+That is, p(x,y) differs from training to testing phase. The interactive plot allows you
+to specify p(x) via a probability tabels while p(y|x) remains fixed.
+You can see that underspecified discriminative models are not immune to covariate shift.
+
+Use the ``bokeh serve`` command to run the interactive plot:
+
+    bokeh serve bokeh_checkerboard.py
+
+at your command prompt. Then navigate to the URL
+
+    http://localhost:5006/bokeh_checkerboard
+
+in your browser.
+"""
+
 import numpy as np
 from collections import OrderedDict
 
